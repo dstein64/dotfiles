db2482b45d15167561ecd24bd0f7429194546478
Dan <dstein64@users.noreply.github.com>
2019-05-06 21:36:37 -0400

    Update install.sh to take packages as arguments.

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Usage: ./install.sh [package1 [package2 ...]]
+# Without specifying any packages, all packages are installed.
+
 set -o errexit
 
 if ! which stow &>/dev/null; then
@@ -9,8 +12,16 @@ fi
 
 TARGET=${HOME}
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 cd ${SCRIPTDIR}/packages
-for package in *; do
+
+PACKAGES=( "$@" )
+if [ ${#PACKAGES[@]} == 0 ]; then
+  PACKAGES=( * )
+fi
+
+for package in "${PACKAGES[@]}"; do
+  echo "installing '${package}'"
   stow --ignore '\.DS_Store' \
        --verbose 1 \
        --target ${TARGET} \
