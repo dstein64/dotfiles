7eb8a64ee218b38c3dce339c898f40c1a936ec6f
Dan <dstein64@users.noreply.github.com>
2021-01-17 00:22:28 -0500

    Add --help to install.sh.

--- install.sh
+++ install.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
-# Usage: ./install.sh [package1 [package2 ...]]
-# Without specifying any packages, all packages are installed.
-
 set -o errexit
-
-if ! which stow &>/dev/null; then
-  echo "missing dependency: stow" >&2
-  exit 1
-fi
 
 TARGET="${HOME}"
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${SCRIPTDIR}/packages"
+
+for arg in "$@"; do
+  if [ "$arg" == '--help' ]; then
+    echo "Usage: $0 [package1 [package2 ...]]"
+    echo 'Without specifying any packages, all packages are installed.'
+    echo 'Packages:'
+    for package in *; do
+      echo "  ${package}"
+    done
+    exit 0
+  fi
+done
+
+if ! which stow &>/dev/null; then
+  echo 'missing dependency: stow' >&2
+  exit 1
+fi
 
 PACKAGES=( "$@" )
 if [ ${#PACKAGES[@]} == 0 ]; then
